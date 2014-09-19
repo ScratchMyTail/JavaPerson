8f560c259eb891908ad90274d4c125c9df044416
christeh <hansen.christer@gmail.com>
2014-09-19 11:19:16 +0200

    Serializable

--- src/no/hinesna/Main.java
+++ src/no/hinesna/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        List<Person> personer = new ArrayList<Person>();
+        List<Person> personer = loadSerializable();
         Scanner scanner = new Scanner(System.in);
         boolean b = true;
 
@@ -26,6 +26,7 @@ public class Main {
             if(valg == 'E'){
                 System.out.println("Avslutter.");
                 b = false;
+                saveSerializable(personer);
             }
             else if(valg == 'A'){
                 System.out.print("Skriv inn fornavn: ");
@@ -57,4 +58,97 @@ public class Main {
             }
         }
     }
+
+    private static void saveCSV(List<Person> personer){
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter("personer.txt"));
+
+            for(Person p : personer){
+                pw.write(p.getForNavn()+","+p.getEtterNavn());
+            }
+            pw.flush();
+            pw.close();
+        }catch(IOException ex){
+            System.out.println("Feil med I/O til fil.");
+        }
+    }
+
+    private static List<Person> loadCSV(){
+        List<Person> personer = new ArrayList<Person>();
+
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader("personer.txt"));
+            String line = bf.readLine();
+
+            while(line != null){
+                String[] fields = line.split(",");
+                personer.add(new Person(fields[0], fields[1]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return personer;
+    }
+
+    private static void saveSerializable(List<Person> personer){
+        try {
+            FileOutputStream fos = new FileOutputStream("personer.ser");
+            ObjectOutputStream oos = null;
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(personer);
+            oos.flush();
+            oos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<Person> loadSerializable(){
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream("personer.ser");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            List<Person> personer = (ArrayList<Person>)ois.readObject();
+            ois.close();
+            fis.close();
+
+            return personer;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<Person>();
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/no/hinesna/PersonGUI.java
@@ -0,0 +1,10 @@
+package no.hinesna;
+
+/**
+ * Created by christerhansen on 19.09.14.
+ */
+public class PersonGUI {
+    public static void main(String[] args){
+        
+    }
+}
